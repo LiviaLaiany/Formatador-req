eb0adb31b640b5935032e121baf0a358ba2bae05
luis.henrique <luisnozeta@gmail.com>
2024-10-14 14:30:54 -0300

    Adição de nome e autorelacionamento

--- app/Models/Modelo.php
+++ app/Models/Modelo.php
@@ -10,11 +10,19 @@ class Modelo extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id', 'mod_json'];
+    protected $fillable = ['user_id', 'mod_json', 'nome', 'mod_base_id'];
 
     protected $casts = ['mod_json' => 'array'];
 
     public function user() {
         return $this->belongsTo(User::class);
+    }
+
+    public function modeloBase() {
+        return $this->belongsTo(Model::class, 'mod_base_id');
+    }
+
+    public function modeloPersonalizado() {
+        return $this->hasMany(Model::class, 'mod_base_id');
     }
 }
