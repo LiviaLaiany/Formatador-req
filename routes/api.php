345d7336755577ade07612dbf4d579e54eee5371
luis.henrique <>
2024-10-19 11:31:55 -0300

    api de Modelos

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjetoController;
+use App\Http\Controllers\ModeloController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     Route::apiResource('projetos', ProjetoController::class);
+    Route::apiResource('modelos', ModeloController::class);
     return $request->user();
 });
 
