e0f23fa977827fe1197933a53f4de9bc47a9274e
luis.henrique <luissshenriqueee@hotmail.com>
2024-11-20 10:37:41 -0300

    Separação do back e do front (Laravel API e React)

---
+++ formatador-front/src/App.js
@@ -0,0 +1,24 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+import {useEffect, useState} from "react"
+import api from "./services/api"
+
+function App() {
+  const [user, setUser] = useState()
+
+  useEffect(() => {
+    api.get("/users/romulo27").then((response) => setUser(response.data)).catch((err) => {
+      console.error("ops! ocorreu um erro" + err)
+    })
+  }, [])
+
+  return (
+    <div className="App">
+      <p>Usuário: {user?.login}</p>
+      <p>Biografia: {user?.bio}</p>
+    </div>
+  );
+}
+
+export default App;
