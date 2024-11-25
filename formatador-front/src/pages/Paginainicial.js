998bae42f8108c265ae8f270b34e2819b561edfc
LiviaLaiany <l.laiany@escolar.ifrn.edu.br>
2024-11-25 11:32:51 -0200

    responsividade certa

--- formatador-front/src/pages/Paginainicial.js
+++ formatador-front/src/pages/Paginainicial.js
@@ -3,6 +3,7 @@ import nome from '../imagens/nome.mp4';
 import video1 from '../imagens/video1.mp4';
 import video3 from '../imagens/video3.mp4';
 import video5 from '../imagens/video5.mp4';
+import Rodape from './Rodape.js';
 import logo from '../imagens/FDR-preto.svg';
 
 export default function Paginainicial(){
@@ -10,14 +11,12 @@ export default function Paginainicial(){
     return(
         <div>
             <div  className='vh-100 ' id='box'>
-                <div className='d-flex py-3 h-25 justify-content-between w-100'>
-                    <video className='d-none d-sm-block w-50 w-sm-100 video' autoPlay loop muted>
+                <div className='d-flex d-sm-flex d-none py-3 justify-content-between w-100'>
+                    <video className='w-25 video' autoPlay loop muted>
                         <source src={video1} className='w-100 h-100 'type="video/mp4"></source>
-
                     </video>
-                    <video className=' w-50 w-sm-25 d-none d-sm-block video' autoPlay loop muted>
+                    <video className='w-25 video' autoPlay loop muted>
                         <source src={video3} className='w-100 h-100 'type="video/mp4"></source>
-
                     </video>
                 </div>
                 <div  className='d-flex align-items-center h-25 justify-content-center nome'>
@@ -32,7 +31,7 @@ export default function Paginainicial(){
                     <button className='btn botoes text-center' id='bt-2'>Cadastre-se</button>
                 </div>
                 <div className='d-flex h-25  justify-content-center w-100'>
-                        <video className=' w-sm-25 w-75 video' autoPlay loop muted>
+                        <video className=' w-sm-25 w-xxl-50 w-75 video' autoPlay loop muted>
                             <source src={video5} className='w-100 h-100 'type="video/mp4"></source>
                         </video>
                 </div>
