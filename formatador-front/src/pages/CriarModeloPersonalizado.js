21fba23907cb6f37a2ed8298dcfaa902acb279b4
anaclara200 <clara.melo1@escolar.ifrn.edu.br>
2024-12-04 21:07:46 -0300

    Criarmodelopersonalizado PRONTO E RESPONSIVO

--- formatador-front/src/pages/CriarModeloPersonalizado.js
+++ formatador-front/src/pages/CriarModeloPersonalizado.js
@@ -53,22 +53,29 @@ export default function CriarModeloPersonalizado() {
     return (
         <div className='box-1 '>
             <Nav/>
-            <div className='vh-100'>
-                <span className='align-self-center d-flex justify-content-center text-light my-4' ><h2  className=' p-1 m-3 fs-2'>Criar Novo Modelo Personalizado</h2></span>
-                
-                <div className='d-flex container-fluid justify-content-center mt-5 align-items-center'>
-                    <div className=' w-50 rounded align-self-center bg-light'>
-                        <div className='justify-content-center d-flex align-items-center w-100'>
-                            <form onSubmit={handleSubmit} className='m-2 container '>
-                                <label className='form-label text-center my-2 container-fluid'>
-                                
-                                    <h3 className=''> Nome do Modelo:  </h3><input type="text" value={nome} onChange={(e) => setNome(e.target.value)} required id='form' className=' form-control'/>
-                                    
-                                </label>
-                                <p>Selecione os campos que deseja no modelo do seu Documento de Requisistos</p>
-                                <p className=''>Obs: os campos já marcados são obrigatórios</p>
-                                <div className='row'>
-                                    <div className='col-md-5 col-12 form-check form-switch'>
+            <div className='p-4 pb-sm-4 min-vh-100 d-flex flex-column '>
+                <span className='align-self-center d-flex justify-content-center text-light my-4'>
+                    <h2 className='fs-2 text-center'>Criar Novo Modelo Personalizado</h2>
+                </span>
+            <div className='d-flex min-w-50 justify-content-center align-self-center'>
+                <div className='w-100 w-md-75  w-lg-50 rounded bg-light p-3'>
+                    <div className='d-flex  justify-content-center align-items-center w-100'>
+                        <form onSubmit={handleSubmit} className='container'>
+                            <label className='form-label my-3 w-100'>
+                                <h3 className='fs-5 text-center'>Nome do Modelo:</h3>
+                                <input
+                                    type="text"
+                                    value={nome}
+                                    onChange={(e) => setNome(e.target.value)}
+                                    required
+                                    className='form-control w-100'
+                                />
+                            </label>
+                            <p className='text-start fs-6'>Selecione os campos que deseja no modelo do seu Documento de Requisitos</p>
+                            <p className='text-muted text-start fs-6'>Obs: os campos já marcados são obrigatórios</p>
+
+                            <div className='row p-3'>
+                                    <div className='col-md-6 col-12 form-check  form-switch'>
                                         <h3>Capa</h3>
                                         {capa.map((campo, index) => (
                                             <div key={index}>
@@ -80,13 +87,11 @@ export default function CriarModeloPersonalizado() {
                                                     onChange={() => toggleCampo(capa, setCapa, index)}
                                                     className='form-check-input'
                                                 />
-                                                <span>{campo.titulo}</span>
+                                                <span className='pe-3 '>{campo.titulo}</span>
                                             </div>
                                         ))}
-                                    </div>
-                                    <div className='col-md-2 d-none d-md-block'></div>
-                                    
-                                    <div className='col-md-5 col-12  form-check form-switch '>
+                                    </div>                                    
+                                    <div className='col-md-6 col-12  pt-md-0 pt-2 form-check form-switch '>
                                         <h3>Conteúdo</h3>
                                         
                                         {conteudo.map((secao, index) => (
@@ -107,15 +112,16 @@ export default function CriarModeloPersonalizado() {
                                     
                                 </div>
                                 <div className="align-items-center justify-content-center m-5 d-flex">
-                                    <button type="submit" className="btn  w-50" id="botao">
+                                    <button type="submit" className="btn  min-w-50" id="botao">
                                         Criar Modelo
                                     </button>
-                                </div>
-                            </form>
-                        </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
+        </div>
+
             
             <Rodape/>
         </div>
